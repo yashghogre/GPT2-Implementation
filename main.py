7a8ac46988751cd3601dc903879dfd69df8ac381
Yash <yashghogre100@gmail.com>
2025-03-08 22:53:01 +0530

    added head and multi head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 from transformers import GPT2Tokenizer
 import math
 
@@ -52,8 +53,56 @@ class PositionalEncoding(nn.Module):
         print(f"positional encoding shape:\t{out.shape}")
         return out
 
+class Head(nn.Module):
+    def __init__(self, head_size, emb_dim=GPT2_CONFIG["emb_dim"], qkv_bias=GPT2_CONFIG["qkv_bias"], device=GPT2_CONFIG["device"]):
+        super().__init__()
+        
+        self.emb_dim = emb_dim
+        self.head_size = head_size
+
+        self.qw = nn.Linear(emb_dim, head_size, bias=qkv_bias, device=device)
+        self.kw = nn.Linear(emb_dim, head_size, bias=qkv_bias, device=device)
+        self.vw = nn.Linear(emb_dim, head_size, bias=qkv_bias, device=device)
+
+    def forward(self, x, device=GPT2_CONFIG["device"]):
+        q = self.qw(x)
+        k = self.kw(x)
+        v = self.vw(x)
+
+        mask = torch.triu(torch.full((x.size(1), x.size(1)), float('-inf'), device=device), diagonal=1)
+
+        qk = q @ k.transpose(-2, -1)
+        scaling = qk * (self.emb_dim ** -0.5)
+        add_mask = scaling + mask
+        scaled_sm = F.softmax(add_mask, dim=-1)
+        qk_v = scaled_sm @ v
+
+        print(f"head shape:\t{qk_v.shape}")
+        return qk_v
+
+class Multi_Head(nn.Module):
+    def __init__(self, emb_dim=GPT2_CONFIG["emb_dim"], n_heads=GPT2_CONFIG["n_heads"], device=GPT2_CONFIG["device"]):
+        super().__init__()
+
+        self.head_size = emb_dim // n_heads
+
+        self.heads = nn.ModuleList([Head(self.head_size) for _ in range(n_heads)])
+        self.lyr = nn.Linear(emb_dim, emb_dim, bias=False, device=device)
+
+    def forward(self, x):
+        out = torch.cat([head(x) for head in self.heads], dim=-1)
+        out = self.lyr(out)
+
+        assert x.shape == out.shape, "positional encoding input shape does not match multi-head output shape"
+        print(f"multi head shape: {out.shape}")
+        return out
+
 
 embd = Embedding()
 x_emb = embd(enc_inp)
 pos_e = PositionalEncoding()
 x_pos = pos_e(x_emb)
+# head = Head(head_size=GPT2_CONFIG["emb_dim"])
+# x_head = head(x_pos)
+m_head = Multi_Head()
+x_m_head = m_head(x_pos)
