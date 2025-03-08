11e1cb87db9501076e8e11897a47a88889d39017
Yash <yashghogre100@gmail.com>
2025-03-08 23:55:14 +0530

    completed GPT2 class

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
+''' Imported Libraries '''
+
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from transformers import GPT2Tokenizer
 import math
 
+''' Config '''
+
 GPT2_CONFIG = {
         "vocab_size": 50257,
         "context_length": 1024,
         "emb_dim": 768,
         "n_heads": 12,
-        "n_layers": 12,
+        "n_blocks": 12,
         "drop_rate": 0.1,
         "qkv_bias": False,
         "device": "cuda" if torch.cuda.is_available() else "cpu"
         }
 
+GPT2_CONFIG["ff_hidden_size"] = 4 * GPT2_CONFIG["emb_dim"]
+
 print(f"device:\t{GPT2_CONFIG['device']}")
+
+''' tokenizer '''
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 text = "My name is Yash"
 enc_inp = tokenizer.encode(text, return_tensors='pt').to(GPT2_CONFIG["device"])
 print(f"input tokens shape:\t{enc_inp.shape}")
+
+''' Embedding '''
 
 class Embedding(nn.Module):
     def __init__(self, vocab_size=GPT2_CONFIG["vocab_size"], emb_dim=GPT2_CONFIG["emb_dim"], device=GPT2_CONFIG["device"]):
@@ -32,6 +42,8 @@ class Embedding(nn.Module):
 
         print(f"embedded tokens shape:\t{out.shape}")
         return out
+
+''' Positional Encoding '''
 
 class PositionalEncoding(nn.Module):
     def __init__(self, max_seq_len=GPT2_CONFIG["context_length"], emb_dim=GPT2_CONFIG["emb_dim"], device=GPT2_CONFIG["device"]):
@@ -52,6 +64,8 @@ class PositionalEncoding(nn.Module):
         assert x.shape == out.shape, "input embedding shape does not match positional encoding shape"
         print(f"positional encoding shape:\t{out.shape}")
         return out
+
+''' Masked Attention Head '''
 
 class Head(nn.Module):
     def __init__(self, head_size, emb_dim=GPT2_CONFIG["emb_dim"], qkv_bias=GPT2_CONFIG["qkv_bias"], device=GPT2_CONFIG["device"]):
@@ -80,6 +94,8 @@ class Head(nn.Module):
         print(f"head shape:\t{qk_v.shape}")
         return qk_v
 
+''' Multi Head Masked Attention '''
+
 class Multi_Head(nn.Module):
     def __init__(self, emb_dim=GPT2_CONFIG["emb_dim"], n_heads=GPT2_CONFIG["n_heads"], device=GPT2_CONFIG["device"]):
         super().__init__()
@@ -94,15 +110,73 @@ class Multi_Head(nn.Module):
         out = self.lyr(out)
 
         assert x.shape == out.shape, "positional encoding input shape does not match multi-head output shape"
-        print(f"multi head shape: {out.shape}")
+        print(f"multi head shape:\t{out.shape}")
+        return out
+
+''' Feed Forward Layer '''
+
+class FeedForward(nn.Module):
+    def __init__(self, emb_dim=GPT2_CONFIG["emb_dim"], ff_hidden_size=GPT2_CONFIG["ff_hidden_size"], device=GPT2_CONFIG["device"]):
+        super().__init__()
+
+        self.lyr_1 = nn.Linear(emb_dim, ff_hidden_size, device=device)
+        self.lyr_2 = nn.Linear(ff_hidden_size, emb_dim, device=device)
+        self.gelu = nn.GELU()
+
+    def forward(self, x):
+        out = self.lyr_1(x)
+        out = self.gelu(out)
+        out = self.lyr_2(out)
+
+        print(f"feed forward shape:\t{out.shape}")
+        return out
+
+''' Transformer Block '''
+
+class Block(nn.Module):
+    def __init__(self, emb_dim=GPT2_CONFIG["emb_dim"], drop_rate=GPT2_CONFIG["drop_rate"]):
+        super().__init__()
+
+        self.m_head = Multi_Head()
+        self.ff = FeedForward()
+        self.ln1 = nn.LayerNorm(emb_dim)
+        self.ln2 = nn.LayerNorm(emb_dim)
+        self.dropout = nn.Dropout(drop_rate)
+
+    def forward(self, x):
+        out = self.ln1(x + self.dropout(self.m_head(x)))
+        out = self.ln2(out + self.dropout(self.ff(out)))
+
+        print(f"block shape:\t{out.shape}")
+        return out
+
+''' GPT2 Model Class '''
+
+class GPT2(nn.Module):
+    def __init__(self, n_blocks=GPT2_CONFIG["n_blocks"], emb_dim=GPT2_CONFIG["emb_dim"], vocab_size=GPT2_CONFIG["vocab_size"], device=GPT2_CONFIG["device"]):
+        super().__init__()
+
+        self.embd = Embedding()
+        self.pos_e = PositionalEncoding()
+
+        self.block_list = nn.ModuleList([Block() for _ in range(n_blocks)])
+        self.lyr = nn.Linear(emb_dim, vocab_size, device=device)
+
+    def forward(self, x):
+        out = self.pos_e(self.embd(x))
+
+        for block in self.block_list:
+            out = block(out)
+
+        out = self.lyr(out)
+
+        print(f"GPT2 shape:\t{out.shape}")
         return out
 
 
-embd = Embedding()
-x_emb = embd(enc_inp)
-pos_e = PositionalEncoding()
-x_pos = pos_e(x_emb)
-# head = Head(head_size=GPT2_CONFIG["emb_dim"])
-# x_head = head(x_pos)
-m_head = Multi_Head()
-x_m_head = m_head(x_pos)
+gpt2 = GPT2()
+gpt2.to(GPT2_CONFIG["device"])
+x_gpt2 = gpt2(enc_inp)
+
+gpt2_params = sum(p.numel() for p in gpt2.parameters())
+print(f"GPT2 Params:\t{gpt2_params}")
